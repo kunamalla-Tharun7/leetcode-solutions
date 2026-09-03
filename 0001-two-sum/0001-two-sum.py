3a7101e254f41bc9a21b2b3f3ce47fe7b5da01e2
kunamalla-Tharun7 <kunamallatarun@gmail.com>
2026-09-03 23:20:00 +0530

    Time: 1 ms (61.24%), Space: 20.4 MB (41.6%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map={}
+        for i in range(0,len(nums)):
+            diff =target-nums[i]
+            if diff in map:
+                return i,map[diff]
+            else:
+                map[nums[i]]=i
